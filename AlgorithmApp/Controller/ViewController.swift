dc417e3be8093006d4cd6f7a95494ed5846bf20d
Keenan <Keenanking555@gmail.com>
2018-10-23 12:47:33 -0600

    Added Steps, and outline style

--- AlgorithmApp/Controller/ViewController.swift
+++ AlgorithmApp/Controller/ViewController.swift
@@ -8,22 +8,43 @@
 
 import UIKit
 
-class AlgorithmViewController: UIViewController {
+class AlgorithmViewController: UIViewController
+{
     @IBOutlet weak var SwiftImage: UIImageView!
+    
     @IBOutlet weak var AlgorithmText: UILabel!
     
-    override func viewDidLoad()
+   public override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        formatAlgorithm()
+    
+    
     }
-
-    override func didReceiveMemoryWarning()
+    private func formatAlgorithm() -> Void
     {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let title : String = "Making a new Algorithm"
+        
+        let stepOne : String = "Step 1: Open up Xcode and click on “Create new Xcode project”."
+        let stepTwo : String = "Step 2: Select the “Single View App” option, when the popup appears, then hit “next”."
+        let stepThree : String = "Step 3: Give the app a name, and then hit “Finish”."
+        let stepFour : String = "Step 4: Create a repository for your app on Github, and make sure you make your first Commit. REMEMBER TO SET YOUR GIT IGNORE TO .xcuserstate."
+        let stepFive : String = "Step 5: Organize your files into the folders of Controller, View, Model, and Resources."
+        
     }
-
+ private func createOutlineStyle() -> NSParagraphStyle
+    {
+        let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        
+    
+        outlineStyle.alignment = .left
+        outlineStyle.defaultTabInterval = 15
+        outlineStyle.firstLineHeadIndent = 20
+        outlineStyle.headIndent = 35
+        
+        return outlineStyle
+    }
 
 }
 
