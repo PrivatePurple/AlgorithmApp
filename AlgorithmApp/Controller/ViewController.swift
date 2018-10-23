bf6e6e34c82b58d22c8c1c3531fe0690797e8ebb
Keenan <Keenanking555@gmail.com>
2018-10-23 12:28:40 -0600

    Added IBOutlets and arranged text and an Image

--- AlgorithmApp/Controller/ViewController.swift
+++ AlgorithmApp/Controller/ViewController.swift
@@ -2,20 +2,24 @@
 //  ViewController.swift
 //  AlgorithmApp
 //
-//  Created by Davis, Keenan on 10/23/18.
+//  Created By Keenan Davis, not like anyone cares.
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class AlgorithmViewController: UIViewController {
+    @IBOutlet weak var SwiftImage: UIImageView!
+    @IBOutlet weak var AlgorithmText: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
